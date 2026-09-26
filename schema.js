ab41f7babd858d9bfc161a0332a02ccdb612cd57
Shubham Kadam <shubhamkadam22100@gmail.com>
2026-09-26 23:56:33 +0530

    new changes commit

--- schema.js
+++ schema.js
@@ -22,3 +22,11 @@ module.exports.reviewSchema = Joi.object({
 
 })
 
+module.exports.reviewSchema = Joi.object({
+    review : Joi.object({
+        rating : Joi.number().required().min(1).max(5), 
+        comment : Joi.string().required()
+    }).required(), 
+
+})
+
